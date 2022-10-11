c4f692b7aec1bad8c295dd035b8be1dade2729dd
yayazhang <yaya.zhang@student.howest.be>
2022-10-11 21:21:32 +0200

    refreshed 7k data

--- data-acquisition/real-estate-scraping.py
+++ data-acquisition/real-estate-scraping.py
@@ -10,6 +10,7 @@ from joblib import Parallel, delayed
 root_url = "https://www.immoweb.be/nl/zoeken/huis-en-appartement/te-koop?countries=BE&page="
 header = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
+
 }
 
 for page in range(1, 334):
@@ -22,17 +23,16 @@ for page in range(1, 334):
     house_json = json1[0]['main'][0]['iw-search']['attributes'][':results']
     house_dict = json.loads(house_json)
 
-    with open('immoweb.csv', 'a', newline="", encoding='utf-8') as f:
+    with open('immowebprovince.csv', 'a', newline="", encoding='utf-8') as f:
         headers = ['id', 'type', 'subtype', 'price', 'transactionType', 'kitchen_type', 'energy_heatingType',
                    'bedroom_count', 'land_surface',
                    'outdoor_terrace_exists', 'outdoor_garden_surface', 'wellnessEquipment_hasSwimmingPool',
-                   'condition_isNewlyBuilt', 'location']
+                   'condition_isNewlyBuilt', 'location', 'region', 'province', 'EPC']
         writer = csv.DictWriter(f, fieldnames=headers)
         writer.writeheader()
 
 
         # to get each house page's url
-
         def flatten(d, parent_key='', sep='_'):
             items = []
             for k, v in d.items():
@@ -56,33 +56,43 @@ for page in range(1, 334):
             link = f"{each_house_base_url}{property_type}/{property_transaction_type}/{property_location}/{property_postcode}/{property_id}"
             link_list.append(link)
 
+            keys = ['id', 'region', 'province', 'EPC']
+            dict_house = collections.defaultdict(list)
+            for key in keys:
+                if key == 'region':
+                    dict_house[key].append(house_dict[0]['property']['location']['region'])
+                elif key == 'province':
+                    dict_house[key].append(house_dict[0]['property']['location']['province'])
+                elif key == 'EPC':
+                    dict_house[key].append(house_dict[0]['transaction']['certificate'])
+                    dict_info = dict(dict_house)
 
-        def scrape(link):
-            # to get each house's information
-            property_request = requests.get(link, headers=header)
-            property_soup = BeautifulSoup(property_request.text, 'html.parser')
 
-            # convert json to dict, add location in the dict
-            script_text = property_soup.find('script', text=re.compile("\s+window.dataLayer")).text.split('= ', 1)[1]
-            json_data = json.loads(script_text[script_text.find('{'):script_text.rfind('}') + 1])
-            property_info = json_data['classified']
-            property_info['location'] = property_location
+            def scrape(link):
+                # to get each house's information
+                property_request = requests.get(link, headers=header)
+                property_soup = BeautifulSoup(property_request.text, 'html.parser')
 
-            # get required info from property_info
-            key_lst = ['id', 'type', 'subtype', 'price', 'transactionType', 'kitchen', 'energy',
-                       'bedroom', 'land',
-                       'outdoor', 'wellnessEquipment', 'condition', 'location']
-            required_property_info = {key: value
-                                      for key, value in property_info.items()
-                                      if key in key_lst}
+                # convert json to dict, add location in the dict
+                script_text = property_soup.find('script', text=re.compile("\s+window.dataLayer")).text.split('= ', 1)[
+                    1]
+                json_data = json.loads(script_text[script_text.find('{'):script_text.rfind('}') + 1])
+                property_info = json_data['classified']
+                property_info['location'] = property_location
+                property_info.update(dict_info)
 
-            # flat the required_property_info
-            # flat_required_property_info = (flatdict.FlatDict(required_property_info, delimiter='.'))
-            flat_required_property_info = flatten(required_property_info)
-            # print(flat_required_property_info)
+                # get required info from property_info
+                key_lst = ['id', 'type', 'subtype', 'price', 'transactionType', 'kitchen', 'energy',
+                           'bedroom', 'land',
+                           'outdoor', 'wellnessEquipment', 'condition', 'location', 'region', 'province', 'EPC']
+                required_property_info = {key: value
+                                          for key, value in property_info.items()
+                                          if key in key_lst}
 
-            # writer.writeheader()
-            writer.writerow(flat_required_property_info, )
+                # flat the required_property_info
+                flat_required_property_info = flatten(required_property_info)
 
+                # print(flat_required_property_info)
+                writer.writerow(flat_required_property_info)
 
         Parallel(n_jobs=-3, require="sharedmem", verbose=10)(delayed(scrape)(link) for link in link_list)
