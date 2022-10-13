1e739b56a1b74115583e0d8d1c7c1e0e38256af5
yayazhang <yaya.zhang@student.howest.be>
2022-10-13 15:59:06 +0200

    needs to fix dataset_4 datatype problem

--- data-acquisition/cleaning-data.py
+++ data-acquisition/cleaning-data.py
@@ -1,13 +1,3 @@
-import pandas as pd
-from IPython.core.display_functions import display
-
-df = pd.read_csv('immowebepc.csv')
-
-# drop column swimmingpool
-df.drop('wellnessEquipment_hasSwimmingPool', axis=1, inplace=True)
-
-# drop duplicate row base on same id
-pd.drop_duplicates(subset=['id'], keep='last')
 
 
 
