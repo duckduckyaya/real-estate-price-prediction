87ab32cfc3f9a5778a6a1bd8f8cf5f66c739ceca
yayazhang <yaya.zhang@student.howest.be>
2022-10-30 02:52:21 +0200

    add remove outlier function

--- model_training/training_model2.py
+++ model_training/training_model2.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+import seaborn as sn
 
 # read csv
 data_file = 'immo.csv'
@@ -11,12 +13,20 @@ df = pd.read_csv(data_file)
 
 # clean data
 df = pd.DataFrame(df)
-df = df[['Type', 'Price', 'NetHabitableSurface(msq)', 'street', 'ConstructionYear', 'BedroomCount', 'locality',
-         'HeatingType', 'KitchekType']]
-columns_to_n = ['Type', 'street', 'locality', 'HeatingType', 'KitchekType']
-columns_to_0 = ['Price', 'NetHabitableSurface(msq)', 'ConstructionYear', 'BedroomCount']
-df[columns_to_n] = df[columns_to_n].replace(np.nan, 'None')
+df = df[['Type', 'Price', 'NetHabitableSurface(msq)','street',  'ConstructionYear','BedroomCount', 'locality',  'HeatingType', 'KitchekType', 'FacadeCount','FloodZoneType', 'IsDoubleGlaze']]
+columns_to_n = ['Type', 'street',  'locality', 'HeatingType', 'KitchekType', 'FloodZoneType']
+columns_to_0 = ['Price',  'BedroomCount']
+df[columns_to_n] = df[columns_to_n].replace(np.nan, 'No Value')
 df[columns_to_0] = df[columns_to_0].replace(np.nan, 0)
+# age of building
+list_years = []
+for year in df['ConstructionYear']:
+    years = 2022 - year
+    list_years.append(years)
+df = df.assign(ConstructionYear = list_years)
+
+df[['NetHabitableSurface(msq)', 'ConstructionYear']] = df[['NetHabitableSurface(msq)','ConstructionYear']].fillna(df[['NetHabitableSurface(msq)', 'ConstructionYear']].mean())
+df['IsDoubleGlaze'] = df['IsDoubleGlaze'].fillna(False)
 
 # encoder
 labelencoder = LabelEncoder()
@@ -25,44 +35,17 @@ df['locality'] = labelencoder.fit_transform(df['locality'])
 df['HeatingType'] = labelencoder.fit_transform(df['HeatingType'])
 df['KitchekType'] = labelencoder.fit_transform(df['KitchekType'])
 df['street'] = labelencoder.fit_transform(df['street'])
+df['FloodZoneType'] = labelencoder.fit_transform(df['FloodZoneType'])
+df['IsDoubleGlaze'] = labelencoder.fit_transform(df['IsDoubleGlaze'])
 
 # X, y
-X = np.array(df[['KitchekType', 'HeatingType', 'street', 'BedroomCount', 'Type', 'ConstructionYear', 'locality',
-                 'NetHabitableSurface(msq)']])
+X = np.array(df[['FloodZoneType','KitchekType','HeatingType','street','BedroomCount','Type', 'ConstructionYear', 'locality', 'NetHabitableSurface(msq)','Price', 'IsDoubleGlaze']])
 y = np.array(df['Price'])
 
 # normalize data
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=41, test_size=0.2)
 X_train = StandardScaler().fit_transform(X_train)
 X_test = StandardScaler().fit_transform(X_test)
-
-# KNN MODEL
-from sklearn.neighbors import KNeighborsClassifier
-
-knn_model = KNeighborsClassifier(n_neighbors=5)
-knn_model.fit(X_train, y_train)
-knn_model.score(X_train, y_train)
-
-knn_predict = knn_model.predict(X_test)
-knn_model.score(X_test, y_test)
-
-# LINEAR REGRESSION
-regression = LinearRegression()
-regression.fit(X_train, y_train)
-regression.score(X_train, y_train)
-
-linear_predict = regression.predict(X_test)
-regression.score(X_test, y_test)
-
-# RANDOM FOREST MODEL
-from sklearn.ensemble import RandomForestClassifier
-
-rt_model = RandomForestClassifier()
-rt_model.fit(X_train, y_train)
-rt_model.score(X_train, y_train)
-
-rt_model.predict(X_test)
-rt_model.score(X_test, y_test)
 
 # SVM MODEL
 from sklearn.svm import SVC
@@ -73,3 +56,10 @@ svc_model.score(X_train, y_train)
 
 svg_predict = svc_model.predict(X_test)
 svc_model.score(X_test, y_test)
+
+# graph
+
+y_predict = svg_predict.predict(X_test)
+cm = confusion_matrix(y_test, y_predict)
+plt.figure(figsize=(18,12))
+sn.heatmap(cm, annot=True)
